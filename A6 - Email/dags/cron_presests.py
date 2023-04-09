1e8c6d12aeea6910fba8ce0cf1c4db592c958e26
Luiggi <luiggi.ms.1@gmail.com>
2023-04-09 13:57:31 -0300

    Correção

--- A6 - Email/dags/cron_presests.py
+++
@@ -1,8 +0,0 @@
-from datetime import datetime
-from airflow import DAG
-from airflow.operators.empty import EmptyOperator
-
-with DAG('cron_presets', start_date=datetime(2023, 1, 1), schedule_interval='@weekly') as dag:
-    task = EmptyOperator(task_id='task1')
-
-task
